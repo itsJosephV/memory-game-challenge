30249a1bfdea83f0bbe91d16dd5e86a88410b990
Joseph <jvp.2703@gmail.com>
2024-05-13 22:28:13 +0100

    fixed files from tsx to ts

--- src/utils/calculateMatches.tsx
+++
@@ -1,5 +0,0 @@
-import {type BoardProps} from "../types";
-
-export const calculateMatches = (boardItems: BoardProps[][]) => {
-  return Math.floor(boardItems.flat().filter((item) => !item.flipped).length / 2);
-};
